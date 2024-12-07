cdead2706a56931d2fe0032fc1a45351faf58e32
gdrffg <gdrffg@gmail.com>
2024-12-07 15:08:14 +0900

    add:spring mvc v3

---
+++ src/main/java/hello/mvcPractice1/springmvc/v3/SpringMemberControllerV3.java
@@ -0,0 +1,42 @@
+package hello.mvcPractice1.springmvc.v3;
+
+import hello.mvcPractice1.domain.member.Member;
+import hello.mvcPractice1.domain.member.MemberRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller()
+@RequestMapping("/springmvc/v3/members")
+public class SpringMemberControllerV3 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @GetMapping("/new-form")
+    public String newForm() {
+        return "new-form";
+    }
+    @GetMapping
+    public String members(Model model) {
+        List<Member> members = memberRepository.findAll();
+        model.addAttribute("members",members);
+        return "members";
+    }
+
+    @RequestMapping("/save")
+    public String save(@RequestParam("username") String username,
+                       @RequestParam("age") int age,
+                       Model model)
+    {
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+        model.addAttribute("member", member);
+        return "save-result";
+    }
+}
